6900867842ff613f172bfe126b648c0b65337fb2
kurumitoishi <bcyq963@gmail.com>
2021-10-21 00:09:51 +0900

    1-3
    1-3

---
+++ JavaStudy/JavaScript/1-3/main.js
@@ -0,0 +1,33 @@
+let a = 10;
+let b = 15;
+let x = 10;
+let y = 0;
+
+
+console.log(a != 10);
+console.log(b >= 10 && b <= 20);
+
+
+if (a != 10){
+    console.log('aは10ではない');
+}else{
+    console.log('aは10です');
+}
+
+if (b > 10 ){
+    console.log('aは10ではない');
+}else{
+    console.log('aは10です');
+}
+
+if (x >= 10 && x <= 20){
+    console.log('成功です');
+}else{
+    console.log('失敗です');
+}
+
+if ( y % 2 == 0){
+    console.log('偶数です');
+}else{
+    console.log('奇数です');
+}
